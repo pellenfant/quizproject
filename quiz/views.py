74f6d7ff740b3f3d6b63e002d6d6f1c157cd4019
pellenfant <ellenfreider@Ellens-MacBook-Air.local>
2016-09-08 11:49:43 +0200

    Lagt in testdata

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
+quizzes = [
+{
+	"quiz_number":1,
+	"name":"Äpplen",
+	"description":"Vad kan du om äpplen?"
+},
+
+{
+	"quiz_number":2,
+	"name":"Elefanter",
+	"description":"Elefantkunskap 1.0"
+},
+{
+	"quiz_number":3,
+	"name": "Internet",
+	"description": "Kan du din internetkuriosa?"
+},
+{
+	"quiz_number":4,
+	"name": "Quizquiz",
+	"description": "Det lilla meta-quizet"
+},
+]
+
 # Create your views here.
 def startpage(request):
 	return render(request,"quiz/start.html")
