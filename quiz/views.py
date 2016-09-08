1415a91190879998811454776080e95479738308
pellenfant <ellenfreider@Ellens-MacBook-Air.local>
2016-09-08 14:16:56 +0200

    Lagt till data på results-sidan.
    Plus ändrat i content.

--- quiz/views.py
+++ quiz/views.py
@@ -50,5 +50,14 @@ def question(request, quiz_number, question_number):
 	return render(request, "quiz/question.html", context)
 
 
-def completed(request):
-	return render (request, "quiz/results.html")
+def completed(request, quiz_number):
+	context = {
+		"correct": 12,
+		"total": 20,
+		"quiz_number": quiz_number,
+	}
+	return render (request, "quiz/results.html", context)
+
+
+
+
